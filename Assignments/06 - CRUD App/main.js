322994afbf044d3fafe3d37607e3d2b66137ef3d
Alexis <acovingt93@gmail.com>
2022-01-28 05:18:55 -0500

    added createData and if else

--- Assignments/06 - CRUD App/main.js
+++ Assignments/06 - CRUD App/main.js
@@ -1,6 +1,11 @@
 var selectedRow = null;
 function onFormSubmit(){
     event.preventDefault();
+    var formData = readFormData();
+    if(selectedRow === null){
+        createNewData(formData);
+    }
+    else{}
 };
 
 function readFormData(){
@@ -20,4 +25,25 @@ function readFormData(){
 
 function createNewData(data){
     var table = document.getElementById("personList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    var cell1 = newRow.insertCell(0);
+        cell1.innerHTML = data.firstName;
+    var cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.middleName;
+    var cell3 = newRow.insertCell(2);
+        cell3.innerHTML = data.lastName;
+    var cell4 = newRow.insertCell(3);
+        cell4.innerHTML = data.email;
+    var cell5 = newRow.insertCell(4);
+        cell5.innerHTML = data.address;
+    var cell6 = newRow.insertCell(5);
+        cell6.innerHTML = data.city;
+    var cell7 = newRow.insertCell(6);
+        cell7.innerHTML = data.state;
+    var cell8 = newRow.insertCell(7);
+        cell8.innerHTML = data.zip;
+    var cell9 = newRow.insertCell(8);
+        cell9.innerHTML = data.dob;   
+    var cell10 = newRow.insertCell(9);
+        cell10.innerHTML = `<button>Edit</button> <button>Delete</button>`;     
 }
