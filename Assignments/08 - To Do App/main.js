e8cb87e19358f5447a6a0db268f128e637271585
Alexis <acovingt93@gmail.com>
2022-02-23 01:30:31 -0500

    addTodo function made but not working

--- Assignments/08 - To Do App/main.js
+++ Assignments/08 - To Do App/main.js
@@ -1,10 +1,32 @@
 $(document).ready( () => {
 
-var newTask = $("#text")
+ //variable for input box   
+var text = $("#text")
+//variable for form
+var form = $("#newTask")
+
+
+function addToDo(){
+    //setting variable for input value
+    let todoText = text.val();
+    //adding new div to list div with the input text
+    $("#list").append(`<div class="toDoItem">
+    <div class="check"><img src="./images/icon-check.svg"></div>
+    <p class="items">${todoText}</p>
+    <div class="cross"><img src="./images/icon-cross.svg"></div>
+</div>  `)
+}
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    addToDo();
+    console.log(`here is ${todoText}`)
+})
+
 
 
 $("#toggle").click( (e) => {
-    e.preventDefault
+    e.preventDefault()
     alert("You clicked toggle")
 })
 //for filter section
