0247882d23fb1dd323a45b82ff2ae763dbe081f1
Alexis <acovingt93@gmail.com>
2022-02-22 23:32:30 -0500

    fixed border radius

--- Assignments/08 - To Do App/main.js
+++ Assignments/08 - To Do App/main.js
@@ -1,3 +1,14 @@
 $(document).ready( () => {
-    
+
+var newTask = $("#text")
+
+
+$("#toggle").click( (e) => {
+    e.preventDefault
+    alert("You clicked toggle")
+})
+//for filter section
+//could use the checkbox to change status
+//if checked, status = complete, else status = active
+//also when checked, text-decoration:line-through
 })
