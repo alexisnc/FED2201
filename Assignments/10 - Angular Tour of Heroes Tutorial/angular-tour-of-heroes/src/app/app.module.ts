6d6f6ea8fba9cc5ad15e99c7bce1a8261ffe58b6
Alexis <acovingt93@gmail.com>
2022-02-27 23:23:47 -0500

    hero app created

--- Assignments/10 - Angular Tour of Heroes Tutorial/angular-tour-of-heroes/src/app/app.module.ts
+++ Assignments/10 - Angular Tour of Heroes Tutorial/angular-tour-of-heroes/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FirstComponentComponent,
-    HeroesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
